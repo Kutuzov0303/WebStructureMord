06893bf4965fd8e3332a1c7c2b96cff1f83590c6
AntonMor <nr.mordezhov@gmail.com>
2026-03-19 19:21:26 +0700

    remove ignored files

--- gallery/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Asset 
-
-@admin.register(Asset)
-class AssetAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at', 'id')
-    search_fields = ('title',)
-    list_filter = ('created_at',)
-    list_display_links = ('title',)
